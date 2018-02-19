52273465fe46daacbfd72e539976b56da367e4f2
zishrodrigues <zish.rodrigues@gmail.com>
2018-02-19 20:27:19 +0100

    added aframe min.js & tested

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 
 gulp.task('js', function(){
   return gulp.src('static/js/*.js')
+  .pipe(concat('app.min.js'))
   .pipe(gulp.dest('build/js'))
 });
 
